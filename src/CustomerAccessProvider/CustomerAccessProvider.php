adeb8e3494a27b44a800ca9ea8cf6c89402e0398
Marlin Cremers <m.cremers@cvo-technologies.com>
2015-10-11 18:13:40 +0200

    Make the access provider model aware

--- src/CustomerAccessProvider/CustomerAccessProvider.php
+++ src/CustomerAccessProvider/CustomerAccessProvider.php
@@ -4,9 +4,24 @@ namespace Webshop\CustomerAccessProvider;
 
 use Cake\Controller\Controller;
 use Cake\Core\App;
+use Cake\Datasource\ModelAwareTrait;
+use Cake\ORM\Locator\LocatorAwareTrait;
 
 class CustomerAccessProvider
 {
+
+    use ModelAwareTrait;
+    use LocatorAwareTrait;
+
+    /**
+     * Constructs the access provider and sets up the model factory
+     */
+    public function __construct()
+    {
+        $this->modelFactory('Table', [$this->tableLocator(), 'get']);
+
+        $this->initialize();
+    }
 
     /**
      * Gets a customer access provider
@@ -20,6 +35,15 @@ class CustomerAccessProvider
         $class = App::className($class, 'CustomerAccessProvider', 'AccessProvider');
 
         return new $class;
+    }
+
+    /**
+     * Initialize the customer access provider
+     *
+     * @return void
+     */
+    public function initialize()
+    {
     }
 
     /**
