97a880cb880dae320f4ce419458b3e99fbd98b22
Marlin Cremers <m.cremers@cvo-technologies.com>
2016-02-08 21:25:57 +0100

    Support component and helper hooking to plugin controller

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -3,22 +3,41 @@
 namespace Webshop\Controller;
 
 use App\Controller\AppController as BaseController;
+use Cake\Event\Event;
+use Cake\Network\Request;
+use Cake\Network\Response;
+use Croogo\Core\Controller\HookableComponentInterface;
+use Croogo\Core\Controller\HookableComponentTrait;
+use Croogo\Core\Croogo;
 use Crud\Controller\Component\CrudComponent;
 use Crud\Controller\ControllerTrait;
 
 /**
  * @property CrudComponent Crud
  */
-class AppController extends BaseController
+class AppController extends BaseController implements HookableComponentInterface
 {
 
     use ControllerTrait;
+    use HookableComponentTrait;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function __construct($request, $response, $name)
+    {
+        parent::__construct($request, $response, $name);
+
+        $this->eventManager()->dispatch(new Event('Controller.afterConstruct', $this));
+    }
 
     /**
      * {@inheritDoc}
      */
     public function initialize()
     {
+        $this->dispatchBeforeInitialize();
+
         parent::initialize();
 
         $this->loadComponent('Crud.Crud', [
@@ -47,5 +66,20 @@ class AppController extends BaseController
         ]);
 
         $this->Crud->addListener('search', 'Search.Search');
+    }
+
+    /**
+     * implementedEvents
+     */
+    public function implementedEvents()
+    {
+        return parent::implementedEvents() + [
+            'Controller.afterConstruct' => 'afterConstruct',
+        ];
+    }
+
+    public function afterConstruct()
+    {
+        $this->viewBuilder()->helpers(Croogo::options('Hook.view_builder_options', $this, 'helpers'));
     }
 }
